6a75452b7be613c00cf0fbf4708521c4d089fd0d
jasonslepicka <jasonslepicka@gmail.com>
2016-11-02 19:03:14 -0700

    Update to dig extractor 0.3.x

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ config = {
     'url': 'https://github.com/usc-isi-i2/dig-readability-extractor',
     'download_url': 'https://github.com/usc-isi-i2/dig-readability-extractor',
     'author_email': 'jasonslepicka@gmail.com',
-    'version': '0.2.0',
+    'version': '0.3.0',
     # these are the subdirs of the current directory that we care about
     'packages': ['digReadabilityExtractor', 'digReadabilityExtractor.readability'],
     'scripts': [],
-    'install_requires': ['digExtractor>=0.2.0','lxml>=3.6.0','beautifulsoup4>=4.3','cssselect','chardet']
+    'install_requires': ['digExtractor>=0.3.2','lxml>=3.6.0','beautifulsoup4>=4.3','cssselect','chardet']
 }
 
 setup(**config)
